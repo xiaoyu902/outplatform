6d7e55cbba1afc6cc5ff6331b97e7e3f006f5543
liucong <liuconglook@gmail.com>
2020-11-09 13:39:19 +0800

    清空服务名

--- outplatform-provider-manager-9001/src/main/java/com/rongli/mapper/primary/PatientMapper.java
+++ outplatform-provider-manager-9001/src/main/java/com/rongli/mapper/primary/PatientMapper.java
@@ -1,6 +1,5 @@
 package com.rongli.mapper.primary;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.apache.ibatis.annotations.CacheNamespace;
@@ -18,24 +17,5 @@ public interface PatientMapper extends BaseMapper<Patient> {
 	@Select("SELECT * FROM v_api_create WHERE name LIKE #{name} AND term_id LIKE #{termId}")
 	public List<JSONObject> selectListByNameAndTermId(@Param("name") String name, @Param("termId") String termId);
 
-	public static void main(String[] args) {
-		int size = 10; // 数据量
-		int value = 100; // 近似值
-		
-		double[] arr = new double[size];
-		for (int i = 0; i < size; i++) {
-			arr[i] = Math.random() * 1000 - 500; // 随机生成-500 ~ 500
-		}
-		System.out.println(Arrays.toString(arr));
-		double min = Math.abs(arr[0] - value);
-		int index = 0;
-		for(int i = 0; i< size; i++) {
-			if(min > Math.abs(arr[i] - value)) {
-				index = i;
-				min = Math.abs(arr[i] - value);
-			}
-		}
-		System.out.println("最小的绝对值：" + min);
-		System.out.println("与100最接近的值：" + arr[index]);
-	}
+	
 }
